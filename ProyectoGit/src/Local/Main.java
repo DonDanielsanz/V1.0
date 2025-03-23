0fe66109f2fa551d19425400b20ed39f1407c781
Dani <Dani@DANIEL_PC>
2025-03-23 12:45:00 +0100

    Version 1.1

--- ProyectoGit/src/Local/Main.java
+++ ProyectoGit/src/Local/Main.java
@@ -3,7 +3,7 @@ package Local;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Posibles fichajes del betis:");
+		System.out.println("Posibles fichajes del betis en el mercado invernal:");
 		Jugador j1=new Jugador("messi");
 		
 		System.out.println(j1.getNombre());
