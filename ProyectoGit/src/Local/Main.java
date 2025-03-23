5539b6f737b4eb97aa3ca034394add31f8dfef1f
Dani <Dani@DANIEL_PC>
2025-03-23 12:36:33 +0100

    Version nuevo desarrollo

--- ProyectoGit/src/Local/Main.java
+++ ProyectoGit/src/Local/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Posibles fichajes del betis:");
+		Jugador j1=new Jugador("messi");
+		
+		System.out.println(j1.getNombre());
 	}
 
 }
